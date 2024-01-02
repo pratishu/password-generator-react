582c1af0331dd7c6d0e2f3c18906f870ad480dac
pratishu <pratishu.kr@gmail.com>
2024-01-03 02:02:55 +0530

    completed the password generator card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,74 @@
 import { useState } from "react";
 
 function App() {
+  const [length, setLength] = useState(12);
+  const [isNumber, setIsNumber] = useState(false);
+  const [isChar, setisChar] = useState(false);
+  const [password, setPassword] = useState("");
+
   return (
-    <div className=" relative h-screen w-full flex justify-center items-center bg-blue-500/10 text-gray-700">
+    <div className=" relative h-screen w-full flex justify-center items-center bg-gray-50 text-gray-700">
       <div className="flex flex-col w-[1530px] mx-auto h-full items-center justify-evenly gap-2 px-4">
         <h1 className="text-5xl text-sky-700 font-bold  px-4 py-2  ">
           Random Password Generator
         </h1>
-        <div className="min-w-80 min-h-44 w-[800px] h-[300px] rounded-lg shadow-lg bg-slate-100 border border-slate-950/5 "></div>
+        <div className="min-w-80 min-h-44 w-[800px] h-[300px] rounded-lg shadow-lg bg-zinc-50 border border-slate-950/5 ">
+          <div className="flex flex-col justify-between items-center h-full gap-2 py-8">
+            <input
+              type="text"
+              value={password}
+              placeholder="Generated Password"
+              readOnly
+              className="outline-none w-3/4 font-medium text-lg rounded-lg shadow-md px-8 py-4"
+            />
+            <div className="flex justify-evenly w-full px-4">
+              <div className="flex flex-col gap-2 w-1/3 justify-center items-center">
+                <input
+                  type="range"
+                  min={8}
+                  max={30}
+                  value={length}
+                  className="cursor-pointer w-full"
+                  onChange={(e) => setLength(Number(e.target.value))} // normal e.target.value gives type error
+                />
+                <label
+                  htmlFor="length"
+                  className="text-lg font-medium  text-blue-400"
+                >
+                  Length: {length}
+                </label>
+              </div>
+              <div className="flex flex-col gap-2 ">
+                <div className="flex items-center font-medium text-blue-400 text-lg gap-2">
+                  <input
+                    type="checkbox"
+                    defaultChecked={isNumber}
+                    onChange={() => setIsNumber((prev) => !prev)}
+                    className="w-6 h-6 "
+                  />
+                  <label htmlFor="number">Numbers</label>
+                </div>
+                <div className="flex gap-2 text-blue-400 text-lg font-medium">
+                  <input
+                    type="checkbox"
+                    defaultChecked={isChar}
+                    onChange={() => setisChar((prev) => !prev)}
+                    className="w-6 h-6"
+                  />
+                  <label htmlFor="char">Characters</label>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-evenly w-full px-4">
+              <button className="rounded-lg border-2 border-blue-500 hover:scale-105 px-4 py-2 font-bold text-blue-500 shadow-blue-400 hover:bg-blue-500 hover:text-white hover:duration-300">
+                Generate
+              </button>
+              <button className="rounded-lg border-2 border-emerald-500 hover:scale-105 px-4 py-2 font-bold text-emerald-500 shadow-emerald-400 hover:bg-emerald-500 hover:text-white hover:duration-300">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
         <div>
           <ul className="font-medium text-lg text-gray-600 ">
             <li className="font-normal mb-8">
@@ -38,7 +99,7 @@ function App() {
           <p className="font-medium text-2xl">
             Project Code on{" "}
             <a
-              className="font-bold text-sky-700 hover:text-sky-500 duration-150"
+              className="font-bold text-sky-700 hover:text-cyan-400 duration-150"
               href="https://github.com/pratishu/password-generator-react"
               target="_blank"
               rel="noopener noreferrer"
